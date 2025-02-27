7e18f936910ac5cc7b3a7ebd0f83fd6bce250011
wangphun <wangphunkonyakwangphunkonyak@gmail.com>
2025-02-27 23:10:35 +0530

    Added CountryDropDown functionalities

--- my-app/src/Components/Navigation/index.js
+++ my-app/src/Components/Navigation/index.js
@@ -2,6 +2,7 @@ import  Button  from '@mui/material/Button';
 import { IoMdMenu } from "react-icons/io";
 import { FaAngleDown } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+
 const Navigation = () =>{
 
     return (
@@ -9,7 +10,7 @@ const Navigation = () =>{
                          <div className='container'>
                              <div className='row'>
                
-                               <div className='col-sm-3 navPart1'>
+                               <div className='col-sm-2 navPart1'>
                                  <Button className='allCatTab align-items-center'>
                                     <span className='icon1 me-2'><IoMdMenu/></span>
                                    <span className='text'>ALL CATEGORIES</span>
@@ -19,14 +20,14 @@ const Navigation = () =>{
                                </div>
                
                                <div className='col-sm-9 navPart2 d-flex align-items-center'>
-                                <ul className='list list-inline-item w-100'>
-                                    <li className='list list-inline-item'><Link to="/">Home</Link></li>
-                                    <li className='list list-inline-item'><Link to="/">Fashion</Link></li>
-                                    <li className='list list-inline-item'><Link to="/">Electronic</Link></li>
-                                    <li className='list list-inline-item'><Link to="/">Bakery</Link></li>
-                                    <li className='list list-inline-item'><Link to="/">Grocery</Link></li>
-                                    <li className='list list-inline-item'><Link to="/">Blog</Link></li>
-                                    <li className='list list-inline-item'><Link to="/">Contact Us</Link></li>
+                                <ul className='list list-inline-item ms-auto'>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Home</Button></Link></li>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Fashion</Button></Link></li>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Electronic</Button></Link></li>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Bakery</Button></Link></li>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Grocery</Button></Link></li>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Blog</Button></Link></li>
+                                    <li className='list list-inline-item'><Link to="/"><Button>Contact Us</Button></Link></li>
                                 </ul>
                                </div>
                
